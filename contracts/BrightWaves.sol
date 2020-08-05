ef27b23aca2b810bbe6a81fb0bef8a0342601ab8
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-05 08:24:54 -0600

    setup netylify functions

---
+++ contracts/BrightWaves.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.6.8;
+
+// needed for upgradability
+//import "@openzeppelin/upgrades/contracts/Initializable.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
