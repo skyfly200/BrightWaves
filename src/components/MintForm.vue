5a3fe00a3eeef8424ce0a56ed58de7578bda7d70
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-05 14:34:12 -0600

    add a basic form

--- src/components/MintForm.vue
+++ src/components/MintForm.vue
@@ -1,4 +1,11 @@
 <template lang="pug">
+v-card.pa-8
+  v-form
+    h2 Mint a Token
+    v-text-field(label="Title") 
+    v-textarea(label="Description") 
+    v-file-input(label="Art File" type="file")
+    v-btn Upload
 </template>
 
 <script>
