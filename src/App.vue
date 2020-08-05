c5a2432a38c9317001339af67e5d4f493c6192a1
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-05 10:44:10 -0600

    replace default components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MintForm>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MintForm from './components/MintForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MintForm
   }
 }
 </script>
---
+++ src/components/MintForm.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  name: "MintForm",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
