cc2208f53dabae3ffd25efa726ae48eac81a31b8
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-05 15:14:08 -0600

    style metadata stepper

--- src/components/MintForm.vue
+++ src/components/MintForm.vue
@@ -9,23 +9,23 @@ v-stepper(v-model='e1')
   v-stepper-items
     v-stepper-content(step='1')
       v-card.mb-12(color='grey lighten-1' height='200px')
-        h2 Upload Art
+        h2 Select Art
         v-file-input(label="Art File" type="file")
-      v-btn(color='primary' @click='e1 = 2') Continue
+      v-btn(color='primary' @click='e1 = 2') Upload To IPFS
       v-btn(text='') Cancel
     v-stepper-content(step='2')
-      v-card.mb-12(color='grey lighten-1' height='200px')
-        h2 Upload Metadata
+      v-card.mb-12(color='grey lighten-1' height='200px').metadata-fields
+        h2 Enter Metadata
         v-text-field(label="Title") 
         v-text-field(label="Description")
-        v-select(:items="collections" label="Collection")
-      v-btn(color='primary' @click='e1 = 3') Continue
+        v-autocomplete(:items="collections" label="Collection").collection-select
+      v-btn(color='primary' @click='e1 = 3') Upload To IPFS
       v-btn(text='') Cancel
     v-stepper-content(step='3')
       v-card.mb-12(color='grey lighten-1' height='200px')
         h2 Mint a Token
         h4 Gas Fees: 0.-----
-      v-btn(color='primary' @click='e1 = 1') Continue
+      v-btn(color='primary' @click='e1 = 1') Mint
       v-btn(text='') Cancel
 </template>
 
@@ -40,4 +40,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style lang="sass" scoped>
+.metadata-fields
+  height: 100%
+</style>
