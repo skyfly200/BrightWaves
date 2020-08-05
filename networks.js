9fff4298f231dee150f4d79eee7f214dad420e58
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-08-05 08:14:20 -0600

    general setup for oz build

--- networks.js
+++ networks.js
@@ -1,12 +1,35 @@
+const { projectId, mnemonic } = require('./secrets.json')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
 module.exports = {
   networks: {
     development: {
       protocol: 'http',
       host: 'localhost',
-      port: 8545,
-      gas: 5000000,
-      gasPrice: 5e9,
+      port: 7545,
+      gas: 6721975,
+      gasPrice: 2e10,
       networkId: '*',
     },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${projectId}`,
+        ),
+      gasPrice: 10e9,
+      gasLimit: 1000000,
+      networkId: '3',
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${projectId}`,
+        ),
+      gasPrice: 10e9,
+      gasLimit: 750000,
+      networkId: '4',
+    },
   },
-};
+}
